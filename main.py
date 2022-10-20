819c814d06980cabfbbe03a43083b2acb212b8ec
Bruno Barbosa <brb.dev@outlook.com>
2022-10-20 11:19:39 -0400

    add usuario route

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from controllers.HealthController import router as HealthController
+from controllers.UsuarioController import router as UsuarioController
 
 app = FastAPI()
 
 app.include_router(HealthController, tags=["Health"], prefix="/api/health")
+app.include_router(UsuarioController, tags=["Usuário"], prefix="/api/usuario")
