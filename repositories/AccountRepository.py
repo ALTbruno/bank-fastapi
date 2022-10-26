968ae264643068466032e84f32e70bbd64bea9bc
Bruno Barbosa <brb.dev@outlook.com>
2022-10-26 10:45:07 -0400

    refactor: move account methods to the right layer

--- repositories/AccountRepository.py
+++ repositories/AccountRepository.py
@@ -1,5 +1,6 @@
+from hashlib import new
 from decouple import config
-from pymongo import MongoClient
+from pymongo import MongoClient, ReturnDocument
 from bson import ObjectId
 
 from models.AccountModel import AccountModel
@@ -18,23 +19,10 @@ class AccountRepository:
 		return created_account
 
 	def get_all(self):
-		accounts = []
-		for account in accounts_collection.find():
-			accounts.append(self.convert_to_account_find(account))
-		return accounts
+		return accounts_collection.find()
 
 	def find_by_id(self, id: str):
-		account = accounts_collection.find_one({ '_id': ObjectId(id)})
-		return self.convert_to_account_find(account)
-
-	def convert_to_account_find(self, account):
-		return {
-			'id': str(account['_id']),
-			'agency_number': str(account['agency_number']),
-			'account_number': str(account['account_number']),
-			'balance': str(account['balance']),
-			'customer_id': str(account['customer_id'])
-		}
+		return accounts_collection.find_one({ '_id': ObjectId(id)})
 		
 	def exists(self, account_number: str) -> bool:
 		return accounts_collection.count_documents({'account_number': account_number}) > 0
--- services/AccountService.py
+++ services/AccountService.py
@@ -18,11 +18,24 @@ class AccountService:
 		return accountRepository.create(account)
 
 	def get_all(self):
-		return accountRepository.get_all()
+		accounts = []
+		for account in accountRepository.get_all():
+			accounts.append(self.convert_to_account_find(account))
+		return accounts
 
 	def find_by_id(self, id: str):
-		return accountRepository.find_by_id(id)
+		account = accountRepository.find_by_id(id)
+		return self.convert_to_account_find(account)
 
 	def get_balance(self, id: str):
 		account = self.find_by_id(id)
 		return {"balance": account['balance']}
+
+	def convert_to_account_find(self, account):
+		return {
+			'id': str(account['_id']),
+			'agency_number': str(account['agency_number']),
+			'account_number': str(account['account_number']),
+			'balance': str(account['balance']),
+			'customer_id': str(account['customer_id'])
+		}
