ce0c0da018391904a21dbc1ab92f2e3c6bacb091
Bruno Barbosa <brb.dev@outlook.com>
2022-11-03 08:47:21 -0400

    chore: Add customer validators

--- dtos/CustomerSave.py
+++ dtos/CustomerSave.py
@@ -1,4 +1,6 @@
-from pydantic import BaseModel, Field, EmailStr
+import re
+from datetime import datetime
+from pydantic import BaseModel, Field, EmailStr, validator
 
 class CustomerSave(BaseModel):
 	name: str = Field(...)
@@ -6,3 +8,23 @@ class CustomerSave(BaseModel):
 	cpf: str = Field(...)
 	email: EmailStr = Field(...)
 	password: str = Field(...)
+
+	@validator('name')
+	def validate_name_length(cls, name):
+		if len(name) < 3:
+			raise ValueError('Must contain at least 3 characters')
+		return name
+
+	@validator('last_name')
+	def validate_last_name_length(cls, last_name):
+		if len(last_name) < 3:
+			raise ValueError('Must contain at least 3 characters')
+		return last_name
+	
+	@validator('cpf')
+	def validate_cpf(cls, cpf):
+		if not re.match('^([\s\d]+)$', cpf):
+			raise ValueError('Must be numeric')
+		if len(cpf) != 11:
+			raise ValueError('Must be 11 characters long')
+		return cpf
